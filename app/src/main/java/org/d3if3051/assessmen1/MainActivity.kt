d400ee68604b32e7e6761c29ed25604e547e2a63
Fawwaz Maulana <115068689+Fawwazbeebbop@users.noreply.github.com>
2024-04-01 10:42:46 +0700

    First Screen

--- app/src/main/java/org/d3if3051/assessmen1/MainActivity.kt
+++ app/src/main/java/org/d3if3051/assessmen1/MainActivity.kt
@@ -3,13 +3,29 @@ package org.d3if3051.assessmen1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.d3if3051.assessmen1.ui.theme.Assessmen1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +38,63 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WelcomeScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(content: @Composable (Modifier) -> Unit){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+               )
+        }
+    ) { padding ->
+        content(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun WelcomeScreen(){
+    MainScreen { modifier ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(13.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = modifier.size(120.dp).padding(bottom = 16.dp),
+                painter = painterResource(id = R.drawable.wallet),
+                contentDescription = "Wallet Image",
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = stringResource(id = (R.string.introduction)),
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 16.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = stringResource(R.string.start))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     Assessmen1Theme {
-        Greeting("Android")
+        WelcomeScreen()
     }
 }
