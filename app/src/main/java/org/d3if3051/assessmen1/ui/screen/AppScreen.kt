05de3f6b60cc3549a6b07d9be9f99b0313c2bef0
Fawwaz Maulana <115068689+Fawwazbeebbop@users.noreply.github.com>
2024-04-04 01:19:44 +0700

    Final

--- app/src/main/java/org/d3if3051/assessmen1/ui/screen/AppScreen.kt
+++ app/src/main/java/org/d3if3051/assessmen1/ui/screen/AppScreen.kt
@@ -250,7 +250,7 @@ fun ScreenContent(modifier: Modifier){
         ) {
             Text(text = stringResource(R.string.share))
         }
-        Text(text = stringResource(R.string.copyright), modifier = Modifier.padding(top = 8.dp))
+        Text(text = stringResource(R.string.copyright), modifier = Modifier.padding(top = 8.dp), textAlign = TextAlign.Center)
     }
 }
 
